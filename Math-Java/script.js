3e1aeee9931960993a8defd36fd3120fa252f7e3
Resac <joaocasersperandio@gmail.com>
2024-09-09 07:44:41 -0300

    Update script.js

---
+++ Math-Java/script.js
@@ -0,0 +1,28 @@
+let numero = 7.2
+ 
+//Math.round arredondamento comum
+console.log(Marh.round(numero))
+ 
+//Math.ceil arredondamento para cima
+console.log(Math.ceil(numero))
+ 
+//Math.floor arredondamento para baixo
+console.log(Math.floor(numero))
+ 
+//Math.pow gera potenciação
+console.log(Math.pow(numero, 3))
+ 
+//Math.sqrt eleva ao quadrado
+console.log(Math.sqrt(numero))
+ 
+//Math.abs gera o número absoluto positivo
+console.log(Math.abs(-5))
+ 
+//Math.min e Math.max encontram o menor e o maior número
+console.log(Math.min(7,45,89,2,125))
+console.log(Math.max(7,45,89,2,125))
+ 
+//Math.random gera um número aleatório
+let numeroAleatorio = Math.floor(Math.random() * 11)
+console.log(numeroAleatorio)
+
