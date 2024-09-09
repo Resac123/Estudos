5a10e8b758756490ed73cea1259e2f51c37eed2a
Resac <joaocasersperandio@gmail.com>
2024-09-09 08:57:06 -0300

    to tentando fazer o joguinho

---
+++ Jogo-Java/script.js
@@ -0,0 +1,7 @@
+const numeroAleatorio = Math.floor(Math.random() * 10) + 1
+const numero = parseInt(document.getElementById("numero").value);
+
+if ( numero = numeroAleatorio)
+    alert("Você acertou")
+
+else alert("Você errou")
