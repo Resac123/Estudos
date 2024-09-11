933f69edb2784507fb57b4a0a6bd18e91c7835c7
Resac <joaocasersperandio@gmail.com>
2024-09-11 12:53:25 -0300

    to tentando

--- Jogo-Java/script.js
+++ Jogo-Java/script.js
@@ -1,8 +1,13 @@
-function adede() {
+function numerasledas(){
     const numeroAleatorio = Math.floor(Math.random() * 10) + 1
-    const numero = parseInt(document.getElementById("numero").value);
+    return numeroAleatorio
+}
 
-    if (numero == numeroAleatorio)
+function adede() { 
+    const numero = parseInt(document.getElementById("numero").value);
+    const pegaNumeroAleatorio = numerasledas()
+
+    if (numero == pegaNumeroAleatorio)
         alert("Você acertou")
 
     else alert("Você errou")
