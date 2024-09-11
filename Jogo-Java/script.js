e606572a110e70167cc9fd2395987d89b1484c8b
Resac <joaocasersperandio@gmail.com>
2024-09-11 12:32:04 -0300

    sei la oq q eu to fazendo

--- Jogo-Java/script.js
+++ Jogo-Java/script.js
@@ -1,7 +1,9 @@
-const numeroAleatorio = Math.floor(Math.random() * 10) + 1
-const numero = parseInt(document.getElementById("numero").value);
+function adede() {
+    const numeroAleatorio = Math.floor(Math.random() * 10) + 1
+    const numero = parseInt(document.getElementById("numero").value);
 
-if ( numero = numeroAleatorio)
-    alert("Você acertou")
+    if (numero == numeroAleatorio)
+        alert("Você acertou")
 
-else alert("Você errou")
+    else alert("Você errou")
+}
