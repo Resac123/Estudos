2febc24c4b66c448427b81410f1ff5fd29c76b8e
Resac <joaocasersperandio@gmail.com>
2024-10-14 08:29:57 -0300

    asdfasdf

---
+++ To-do/script.js
@@ -0,0 +1,44 @@
+function soma(numero1,numero2){
+
+    resultado = numero1 + numero2
+    return resultado
+
+}
+
+console.log(soma(5, 10))
+soma(8, 18)
+
+const personagens = ['Steven', 'Ametista', 'Perola', 'Peridote']
+
+personagens.push('Garnet')
+
+console.log(` O nome do personagem é: ${personagens[2]} `)
+
+function adicionar(){
+
+    const personagens = ['Steven', 'Ametista', 'Perola', 'Peridote']
+    const novoPersonagem = document.getElementById('personagem').value
+    
+    // //Adicionar um personagem no fim da lista
+    //personagens.push(novoPersonagem)
+    
+    // //Adicionar um personagem no comeco da lista
+    //personagens.unshift(novoPersonagem)
+
+    // //exclui o ultimo
+    //personagens.pop()
+
+    // //exclui o primeiro
+    //personagens.shift()
+
+    // //atualizar um array
+    //personagens[2] = 'garnet'
+
+    console.log(personagens)
+
+    personagens.forEach( function (pegaPersonagem) {
+        console.log(`O nome do personagem é: ${pegaPersonagem}`)
+    })
+
+    console.log(personagens)
+}
